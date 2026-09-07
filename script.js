a0e436f197c9f21daf589ecbf64f8cb5cd007e47
Jessica Wan <j27wan@gmail.com>
2026-09-07 23:01:12 +0100

    Full invitation below

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 $(document).ready(function () {
   var envelope = $("#envelope");
+  var invitation = $("#invitation");
+  var rsvpButton = $("#rsvp-button");
+  var revealTimer;
 
-  envelope.click(function () {
-    envelope.toggleClass("open close");
+  function toggleEnvelope() {
+    var isOpening = envelope.hasClass("close");
+
+    clearTimeout(revealTimer);
+    envelope.toggleClass("open", isOpening).toggleClass("close", !isOpening);
+    envelope.attr("aria-expanded", isOpening);
+
+    if (isOpening) {
+      envelope.attr("aria-label", "Close birthday invitation");
+      revealTimer = setTimeout(function () {
+        invitation.addClass("is-visible").attr("aria-hidden", "false");
+        invitation[0].scrollIntoView({ behavior: "smooth", block: "start" });
+      }, 1600);
+    } else {
+      envelope.attr("aria-label", "Open birthday invitation");
+      invitation.removeClass("is-visible").attr("aria-hidden", "true");
+    }
+  }
+
+  envelope.on("click", toggleEnvelope);
+  envelope.on("keydown", function (event) {
+    if (event.key === "Enter" || event.key === " ") {
+      event.preventDefault();
+      toggleEnvelope();
+    }
+  });
+
+  rsvpButton.on("click", function () {
+    rsvpButton.text("Wonderful - you're on the list!").prop("disabled", true);
   });
 });
