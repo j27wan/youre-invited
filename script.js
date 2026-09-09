b2a33fbc1297ecd2d95c33a7fc3ec1a60fde7c00
Jessica Wan <j27wan@gmail.com>
2026-09-09 23:02:36 +0100

    add audio and prevent clipping

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 $(document).ready(function () {
   var envelope = $("#envelope");
   var invitation = $("#invitation");
+  var imageModal = $("#image-modal");
+  var imageModalClose = $(".image-modal-close");
   var revealTimer;
+
+  function playPaperSound() {
+    var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    var buffer = audioContext.createBuffer(1, audioContext.sampleRate * 0.18, audioContext.sampleRate);
+    var data = buffer.getChannelData(0);
+
+    for (var index = 0; index < data.length; index += 1) {
+      data[index] = (Math.random() * 2 - 1) * (1 - index / data.length);
+    }
+
+    var source = audioContext.createBufferSource();
+    var filter = audioContext.createBiquadFilter();
+    var gain = audioContext.createGain();
+    source.buffer = buffer;
+    filter.type = "bandpass";
+    filter.frequency.value = 1800;
+    filter.Q.value = 0.7;
+    gain.gain.setValueAtTime(0.0001, audioContext.currentTime);
+    gain.gain.exponentialRampToValueAtTime(0.08, audioContext.currentTime + 0.02);
+    gain.gain.exponentialRampToValueAtTime(0.0001, audioContext.currentTime + 0.18);
+    source.connect(filter).connect(gain).connect(audioContext.destination);
+    source.start();
+    source.stop(audioContext.currentTime + 0.18);
+  }
 
   function toggleEnvelope() {
     var isOpening = envelope.hasClass("close");
@@ -11,6 +37,7 @@ $(document).ready(function () {
     envelope.attr("aria-expanded", isOpening);
 
     if (isOpening) {
+      playPaperSound();
       envelope.attr("aria-label", "Close birthday invitation");
       revealTimer = setTimeout(function () {
         invitation.addClass("is-visible").attr("aria-hidden", "false");
@@ -27,6 +54,29 @@ $(document).ready(function () {
     if (event.key === "Enter" || event.key === " ") {
       event.preventDefault();
       toggleEnvelope();
+    }
+  });
+
+  function closeImageModal() {
+    imageModal.attr("hidden", true).removeClass("is-open");
+    $("body").removeClass("modal-open");
+  }
+
+  $(".invitation-zoom").on("click", function () {
+    imageModal.removeAttr("hidden").addClass("is-open");
+    $("body").addClass("modal-open");
+    imageModalClose.trigger("focus");
+  });
+
+  imageModalClose.on("click", closeImageModal);
+  imageModal.on("click", function (event) {
+    if (event.target === imageModal[0]) {
+      closeImageModal();
+    }
+  });
+  $(document).on("keydown", function (event) {
+    if (event.key === "Escape" && imageModal.hasClass("is-open")) {
+      closeImageModal();
     }
   });
 
